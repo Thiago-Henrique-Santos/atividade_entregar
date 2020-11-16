852d36ee2dd77803d9beddcc0b38ec30592fd0b6
Meus Documentos <Meus Documentos@MeusDocumentos>
2020-11-16 18:19:20 -0200

    criei uma classe escrito ("Olá Saamantha Gabrielly Barroso")

--- src/Classe_Samantha.java
+++ src/Classe_Samantha.java
@@ -1,5 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Meus Documentos
+ */
 public class Classe_Samantha {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Olá Samantha Gabrielly Barroso");
-    }
+}
 }
