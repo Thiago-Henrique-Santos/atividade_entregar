738f1cd56190766b100c6524e6865d23c63b0054
ThiagoHenriqueSantos <thilego418@gmail.com>
2020-11-16 18:17:22 -0300

    Adicionei o nome da Samantha na Classe_Thiago

--- src/Classe_Thiago.java
+++ src/Classe_Thiago.java
@@ -5,6 +5,7 @@ public class Classe_Thiago {
         System.out.println("Olá, Thiago Henrique dos Santos");
         System.out.println("Olá, Julia Corol dos Santos");
         System.out.println("Olá Natan Fernandes dos Santos");
+        System.out.println("Olá Samantha Gabrielly Barroso");
    }
     
 }
