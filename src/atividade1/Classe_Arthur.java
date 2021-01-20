51ea7416332d217229f6555bb5cf8c874af9f4c5
Arthur Oliveira <Arthur Oliveira@ACER>
2021-01-20 13:14:52 -0300

    Teste classe Arthur

--- src/atividade1/Classe_Arthur.java
+++ src/atividade1/Classe_Arthur.java
@@ -11,6 +11,7 @@
 public class Classe_Arthur {
     public static void main(String[] args) {
         System.out.println("Olá Arthur Hélio de Oliveira Mathias.");
+        System.out.println("Teste");
         System.out.println("Olá, Thiago Henrique dos Santos");
         System.out.println("Olá, Julia Corol dos Santos");
         System.out.println("Olá, Natan Fernandes dos Santos.");
